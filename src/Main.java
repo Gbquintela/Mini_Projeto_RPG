59ad2c843651670861c6c3433123de84de01fbb0
Gabriel Quintela <gabriel_martinhoq@hotmail.com>
2025-03-06 11:28:22 -0300

    Main program file where I just call the Controller that has all the code interaction. Leaving my main file clean

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import Controller.MainMenuController;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Cria uma instância do controlador do menu principal
+        MainMenuController controller = new MainMenuController();
+        // Inicia o menu principal
+        controller.mainMenu();
+    }
+}
